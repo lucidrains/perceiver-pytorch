5eee4b9c64a64c922c55c9a8698f0e989d1fd1f7
Phil Wang <lucidrains@gmail.com>
2021-03-10 22:24:07 -0800

    fix __init__

---
+++ perceiver_pytorch/__init__.py
@@ -0,0 +1,1 @@
+from perceiver_pytorch.perceiver_pytorch import Perceiver
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'perceiver-pytorch',
   packages = find_packages(),
-  version = '0.0.4',
+  version = '0.0.5',
   license='MIT',
   description = 'Perceiver - Pytorch',
   author = 'Phil Wang',
