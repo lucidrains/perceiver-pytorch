ec2395c01cdc8ad210c2f12b411a549ac7f89d77
Phil Wang <lucidrains@gmail.com>
2021-03-21 08:13:14 -0700

    switch to rmsnorm

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'perceiver-pytorch',
   packages = find_packages(),
-  version = '0.1.2',
+  version = '0.1.4',
   license='MIT',
   description = 'Perceiver - Pytorch',
   author = 'Phil Wang',
