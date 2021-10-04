483a6edc85f1d935dd597a71700a3d1fe8d932ac
Phil Wang <lucidrains@gmail.com>
2021-10-04 15:57:25 -0700

    move frequency bands to linear space

--- perceiver_pytorch/perceiver_pytorch.py
+++ perceiver_pytorch/perceiver_pytorch.py
@@ -33,11 +33,11 @@ def fourier_encode(x, max_freq, num_bands = 4):
     x = x.unsqueeze(-1)
     device, dtype, orig_x = x.device, x.dtype, x
 
-    scales = torch.logspace(0., log(max_freq / 2) / log(10), num_bands, device = device, dtype = dtype)
+    scales = torch.linspace(1., max_freq / 2, num_bands, device = device, dtype = dtype)
     scales = scales[(*((None,) * (len(x.shape) - 1)), Ellipsis)]
 
     x = x * scales * pi
-    x = torch.cat([x.sin(), x.cos()], dim=-1)
+    x = torch.cat([x.sin(), x.cos()], dim = -1)
     x = torch.cat((x, orig_x), dim = -1)
     return x
 
--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'perceiver-pytorch',
   packages = find_packages(),
-  version = '0.7.3',
+  version = '0.7.4',
   license='MIT',
   description = 'Perceiver - Pytorch',
   author = 'Phil Wang',
