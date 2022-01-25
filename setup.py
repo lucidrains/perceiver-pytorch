c7a0ef50fce2bd54b2a4eabecb4f531b5caa2856
Phil Wang <lucidrains@gmail.com>
2022-01-24 16:53:35 -0800

    0.8.2

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
   name = 'perceiver-pytorch',
   packages = find_packages(),
-  version = '0.8.1',
+  version = '0.8.2',
   license='MIT',
   description = 'Perceiver - Pytorch',
   author = 'Phil Wang',
